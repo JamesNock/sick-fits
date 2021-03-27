740179be6b74625e2f78fca8a87e66bbccbd77cc
James Nock <james@james-nock.co.uk>
2021-03-27 17:19:11 +0000

    gitignore .next folder

--- backend/.keystone/admin/.next/static/chunks/webpack.js
+++ backend/.keystone/admin/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "88a4acc3e9fcb66e57c8";
+/******/ 	var hotCurrentHash = "bd9a20e34f95c3ded2aa";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
