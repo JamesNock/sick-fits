56a22448d75e0bc4971ff07c5fd2aa619b5a9739
James Nock <james@james-nock.co.uk>
2021-04-04 14:57:43 +0100

    Fix signout import casing

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { useCart } from '../lib/CartState';
 import CartCount from './CartCount';
-import SignOut from './Signout';
+import SignOut from './SignOut';
 import NavStyles from './styles/NavStyles';
 import { useUser } from './User';
 
